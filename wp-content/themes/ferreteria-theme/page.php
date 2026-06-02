f454fa05b33acdf8ee73e635af5c576f14e46a5b
nicog <nicolasgomez94@gmail.com>
2026-06-02 16:56:30 -0300

    Mejora estilos de pagina mayorista

--- wp-content/themes/ferreteria-theme/page.php
+++ wp-content/themes/ferreteria-theme/page.php
@@ -8,19 +8,26 @@ if (! defined('ABSPATH')) {
 }
 
 get_header();
+
+$page_classes = array('section-light', 'page-content-shell');
+
+if (is_page('mayorista')) {
+    $page_classes[] = 'mayorista-page';
+}
 ?>
 
-<main class="section-light">
+<main class="<?php echo esc_attr(implode(' ', $page_classes)); ?>">
   <div class="container">
+    <div class="wp-page-content fade-in">
     <?php
     while (have_posts()) :
         the_post();
         the_content();
     endwhile;
     ?>
+    </div>
   </div>
 </main>
 
 <?php
 get_footer();
-
